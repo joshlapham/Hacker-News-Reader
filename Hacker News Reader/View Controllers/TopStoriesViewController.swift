483405bf1039d9d809dd6181ec652aea8993eb69
Josh Lapham <josh@joshlapham.com>
2015-07-26 09:52:44 +1000

    Add TopStoriesViewController class to project.

---
+++ Hacker News Reader/View Controllers/TopStoriesViewController.swift
@@ -0,0 +1,14 @@
+//
+//  TopStoriesViewController.swift
+//  Hacker News Reader
+//
+//  Created by Josh Lapham on 26/07/2015.
+//  Copyright © 2015 Nerd Burger Labs. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TopStoriesViewController: UITableViewController {
+    
+}

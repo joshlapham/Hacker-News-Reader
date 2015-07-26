b6614e223ab9f66a6c8c7f3257bbe395b54cd5ac
Josh Lapham <josh@joshlapham.com>
2015-07-26 10:05:40 +1000

    Add Obj-C bridging header to project.

---
+++ Hacker News Reader/HNR-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  HNR-Bridging-Header.h
+//  Hacker News Reader
+//
+//  Created by Josh Lapham on 26/07/2015.
+//  Copyright © 2015 Nerd Burger Labs. All rights reserved.
+//
+
+#import <Firebase/Firebase.h>

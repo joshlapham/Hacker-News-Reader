fa30863892d9e9d026e450857c47d3fe577eb2f5
Josh Lapham <josh@joshlapham.com>
2015-07-26 10:05:59 +1000

    Continue implementing fetchTopStories method on Top Stories VC.

--- Hacker News Reader/View Controllers/TopStoriesViewController.swift
+++ Hacker News Reader/View Controllers/TopStoriesViewController.swift
@@ -31,6 +31,63 @@ extension TopStoriesViewController {
     
     // Fetch top story IDs
     func fetchTopStoryIDs() {
+        // Number of items to fetch
+        let itemCountToFetch = 100
+        
+        // Init API URL
+        let hackerNewsAPIURL = "https://hacker-news.firebaseio.com/v0/topstories"
+        
+        // Init Firebase
+        let firebaseRef = Firebase(url: hackerNewsAPIURL)
+        
+        //            DDLogVerbose(@"%s - fetching top item IDs ..", __func__);
+        //
+        //            // Number of items to fetch
+        //            static NSUInteger itemCountToFetch = 100;
+        //
+        //            // Init API URL
+        //            NSString *hnApiUrl = @"https://hacker-news.firebaseio.com/v0/topstories";
+        //
+        //            // Init Firebase
+        //            Firebase *hnRef = [[Firebase alloc] initWithUrl:hnApiUrl];
+        //
+        //            // Fetch data
+        //            [hnRef observeSingleEventOfType:FEventTypeValue
+        //            withBlock:^(FDataSnapshot *snapshot) {
+        //            // Init topItems array
+        //            NSMutableArray *topItems = [NSMutableArray array];
+        //
+        //            // Loop over data to get top story IDs
+        //            for (FDataSnapshot *childSnap in snapshot.children) {
+        //            // Only fetch itemsCountToFetch
+        //            if (topItems.count == itemCountToFetch) {
+        //            break;
+        //            }
+        //
+        //            // Item position
+        //            NSString *topItemPosition = (NSString *)childSnap.name;
+        //
+        //            // Item ID
+        //            NSString *topItemId = [NSString stringWithFormat:@"%@", childSnap.value];
+        //
+        //            //                                  NSLog(@"%s - ITEM: %@; %@", __func__, topItemPosition, topItemId);
+        //
+        //            // Init item dict
+        //            NSDictionary *itemDict = @{
+        //            kApiKeyPosition : topItemPosition,
+        //            kApiKeyId : topItemId
+        //            };
+        //
+        //            // Add top story dict to topStories array
+        //            [topItems addObject:itemDict];
+        //            }
+        //            
+        //            DDLogVerbose(@"%s - top items count: %lu", __func__, (unsigned long)topItems.count);
+        //            
+        //            // Pass topItems array to block
+        //            block(topItems);
+        //            }];
+        
         
     }
 }

f17625ca7b1d500f9bb089c749656ccf44a30140
Josh Lapham <josh@joshlapham.com>
2015-07-27 18:44:45 +1000

    Update Core Data stack init on App Delegate to handle lightweight migrations.

--- Hacker News Reader/AppDelegate.swift
+++ Hacker News Reader/AppDelegate.swift
@@ -65,12 +65,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
         // The persistent store coordinator for the application. This implementation creates and returns a coordinator, having added the store for the application to it. This property is optional since there are legitimate error conditions that could cause the creation of the store to fail.
+        // Init options for persistent store to handle lightweight migrations
+        let persistentStoreOptions = [ NSMigratePersistentStoresAutomaticallyOption: true, NSInferMappingModelAutomaticallyOption: true ]
+        
         // Create the coordinator and store
         let coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
         let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent("SingleViewCoreData.sqlite")
         var failureReason = "There was an error creating or loading the application's saved data."
+        
         do {
-            try coordinator.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: nil)
+            try coordinator.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: persistentStoreOptions)
         } catch {
             // Report any error we got.
             var dict = [String: AnyObject]()
@@ -78,7 +82,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             dict[NSLocalizedFailureReasonErrorKey] = failureReason
             
             dict[NSUnderlyingErrorKey] = error as NSError
-            let wrappedError = NSError(domain: "YOUR_ERROR_DOMAIN", code: 9999, userInfo: dict)
+            let wrappedError = NSError(domain: NSBundle.mainBundle().bundleIdentifier!, code: 9999, userInfo: dict)
             // Replace this with code to handle the error appropriately.
             // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
             NSLog("Unresolved error \(wrappedError), \(wrappedError.userInfo)")
